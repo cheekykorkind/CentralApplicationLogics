cccd76c5c9bc6f2d04ff4b31654e59ad97363548
cheekykorkind <cheekorkind@gmail.com>
2018-01-18 01:44:32 +0900

    HTML과 JavaScript만으로 페이지 이동하기

---
+++ JavaScript/pageNavigation/page1.js
@@ -0,0 +1,15 @@
+/*
+		1. 테스트 명 : 페이지 이동하기
+		2. 결과 표시 : 
+			1) HTML : 표시되는 HTML 파일이 변경된다.
+			2) debugger-ui : 해당없음.
+			
+		3. 알게된 점 : 해당없음.
+*/
+
+function goToPage2() {
+	window.location.href = "page2.html"; 
+}
+function goToPage3() {
+	window.location.href = "page3.html"; 
+}
