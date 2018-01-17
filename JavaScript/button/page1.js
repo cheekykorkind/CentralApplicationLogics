54c5d760352f81085c41dee8fb085430f9649fc7
cheekykorkind <cheekorkind@gmail.com>
2018-01-18 01:34:07 +0900

    버튼 이벤트 처리

---
+++ JavaScript/button/page1.js
@@ -0,0 +1,12 @@
+/*
+		1. 테스트 명 : 버튼 onClick 이벤트 처리하기
+		2. 결과 표시 : 
+			1) HTML : H1의 텍스트가 변경됨.
+			2) debugger-ui : 해당없음.
+			
+		3. 알게된 점 : 해당없음.
+*/
+
+function changeSectionName() {
+	document.getElementById("sectionName").innerText = 'changed!';
+}
